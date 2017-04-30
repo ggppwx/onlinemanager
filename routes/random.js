375fda5c1a5c23e3c5fac87cf438576dbabc5fd4
roy gu <sirgujingwei@gmail.com>
2017-04-30 18:06:13 -0400

    fix

--- routes/random.js
+++ routes/random.js
@@ -8,21 +8,23 @@ var LOCAL_EVENTS_MAP = {};
 var saveEventsOnTags = function(events){
 	var eventsMap = {};
 	for (var i = 0 ; i < events.length; ++i) {
-		for( var j = 0; j < events[i].tags.length; ++j ) {
-			if (events[i].tags[j] in eventsMap){
-				eventsMap[events[i].tags[j]].push(events[i]);
-			} else {
-				eventsMap[events[i].tags[j]] = [ events[i] ];
+		if (events[i].tags) {
+			for( var j = 0; j < events[i].tags.length; ++j ) {
+				if (events[i].tags[j] in eventsMap){
+					eventsMap[events[i].tags[j]].push(events[i]);
+				} else {
+					eventsMap[events[i].tags[j]] = [ events[i] ];
+				}
 			}
 		}
 	}
 	eventsMap['All'] = events;
 	// console.log(eventsMap);
-	return eventsMap; 
+	return eventsMap;
 };
 
 router.get('/event', (req, res, next) => {
-	// do a random generator 
+	// do a random generator
 	Events.find((err, events) => {
 		LOCAL_EVENTS_MAP = saveEventsOnTags(events);
 		res.json(events);
@@ -35,15 +37,15 @@ router.post('/event/random', (req, res, next) => {
 	console.log(req.body.tag);
 	var tag = req.body.tag;
 
-	// based on tag, random a event 	
-	if (!(tag in LOCAL_EVENTS_MAP)) { 
+	// based on tag, random a event
+	if (!(tag in LOCAL_EVENTS_MAP)) {
 		res.json({});
 		return;
 	}
 
 	var len = LOCAL_EVENTS_MAP[tag].length;
 	// 0 ~ len - 1
-	
+
 	if (len !== 0) {
 		var index = Math.floor(Math.random() * len);
 		console.log(index);
@@ -52,8 +54,8 @@ router.post('/event/random', (req, res, next) => {
 		res.json(event);
 	} else {
 		res.end();
-	}	
-	
+	}
+
 	res.end();
 });
 
@@ -62,18 +64,18 @@ router.post('/event/add', (req, res, next) => {
 	var eventContent = req.body.event
 
 	function processEvent(content){
-		// process the string 
+		// process the string
 		var pattern = /#\S+/g;
 		var hashtags = content.match(pattern);
 		var content = content.replace(pattern,"");
 		return { hashtags : hashtags, content : content } ;
 	}
-	// parse the event. 
+	// parse the event.
 	// get the event + tags
     var  eventObj = processEvent(eventContent);
     console.log(eventObj);
 
-	// go to database 
+	// go to database
 	var event = new Events({event : eventObj.content, tags: eventObj.hashtags});
 	event.save(function (err, event){
 		if(err) {
@@ -97,7 +99,7 @@ router.post('/event/add', (req, res, next) => {
 
 router.delete('/event/:id', (req, res) => {
 	console.log(req.params.id);
-	
+
 	Events.remove({_id : req.params.id}, (err, event) => {
 		if (err){
 			console.log("Error in removing event");
@@ -110,7 +112,7 @@ router.delete('/event/:id', (req, res) => {
 			LOCAL_EVENTS_MAP = saveEventsOnTags(events);
 			res.json(events);
 		});
-		
+
 	});
 
 });
